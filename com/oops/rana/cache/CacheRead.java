de481aaf010b6db729c045f75784ea291b48779f
pramod-rana7 <pramod-rana7@users.noreply.github.com>
2014-10-06 16:56:42 +0530

    Update CacheRead.java
    Comment added

--- com/oops/rana/cache/CacheRead.java
+++ com/oops/rana/cache/CacheRead.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
 
 /**
  * @author pramod_rana7
- *
+ * Read data from the xml file
  */
 public class CacheRead implements Serializable{
 
@@ -21,6 +21,7 @@ public class CacheRead implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	/**
+	 * Parametrize Constructor
 	 * @param file in which object is written
 	 * @throws FileNotFoundException 
 	 */
@@ -28,10 +29,16 @@ public class CacheRead implements Serializable{
 		decode =new XMLDecoder(new FileInputStream(paramFile));
 	}
 	
+	/**
+	 * @return Object read from the file
+	 */
 	public Object readObject(){
 		return this.decode.readObject();
 	}
 	
+	/**
+	 * Closing the CacheRead 
+	 */
 	public void close(){
 		if(this.decode!=null){
 			this.decode.close();
